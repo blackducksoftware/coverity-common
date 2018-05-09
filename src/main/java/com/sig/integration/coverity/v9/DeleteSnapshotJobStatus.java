f049997121d27aa0981789a96a622fcf8805d287
jrichard <jrichard@synopsys.com>
2018-05-09 11:03:26 -0400

    Adding the v9 soap sdk for Coverity

---
+++ src/main/java/com/sig/integration/coverity/v9/DeleteSnapshotJobStatus.java
@@ -0,0 +1,39 @@
+package com.sig.integration.coverity.v9;
+
+import javax.xml.bind.annotation.XmlEnum;
+import javax.xml.bind.annotation.XmlType;
+
+/**
+ * <p>Java class for deleteSnapshotJobStatus.
+ *
+ * <p>The following schema fragment specifies the expected content contained within this class.
+ * <p>
+ * <pre>
+ * &lt;simpleType name="deleteSnapshotJobStatus">
+ *   &lt;restriction base="{http://www.w3.org/2001/XMLSchema}string">
+ *     &lt;enumeration value="QUEUED"/>
+ *     &lt;enumeration value="RUNNING"/>
+ *     &lt;enumeration value="SUCCEEDED"/>
+ *     &lt;enumeration value="FAILED"/>
+ *   &lt;/restriction>
+ * &lt;/simpleType>
+ * </pre>
+ */
+@XmlType(name = "deleteSnapshotJobStatus")
+@XmlEnum
+public enum DeleteSnapshotJobStatus {
+
+    QUEUED,
+    RUNNING,
+    SUCCEEDED,
+    FAILED;
+
+    public static DeleteSnapshotJobStatus fromValue(String v) {
+        return valueOf(v);
+    }
+
+    public String value() {
+        return name();
+    }
+
+}
---
+++ src/main/java/com/sig/integration/coverity/v9/package-info.java
@@ -0,0 +1,2 @@
+@javax.xml.bind.annotation.XmlSchema(namespace = "http://ws.coverity.com/v9")
+package com.sig.integration.coverity.v9;
